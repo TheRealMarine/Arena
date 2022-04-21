49144c9469f35ad88752aaa8e23c77ff6d02571e
TheRealMarine <panzercrafter0@gmail.com>
2022-04-21 23:19:18 +0200

    Almost final Wave spawn

--- src/main/java/me/pati/arena/Arena.java
+++ src/main/java/me/pati/arena/Arena.java
@@ -5,6 +5,7 @@ import java.sql.Array;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -22,6 +23,17 @@ public final class Arena extends JavaPlugin {
     public class CommandArena implements CommandExecutor {
         boolean ArenaMode = false;
         int round = 0;
+        public int getRandomElement(List<Integer> list)
+        {
+            Random rand = new Random();
+            return list.get(rand.nextInt(list.size()));
+        }
+        public String getRandomElementString(List<String> list)
+        {
+            Random r = new Random();
+            int randomitem = r.nextInt(list.size());
+            return list.get(randomitem);
+        }
         @Override
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
             if (!ArenaMode) {
@@ -35,18 +47,29 @@ public final class Arena extends JavaPlugin {
             }
             return false;
         }
-        public void spawnWave() {
-            int maxDifferentMobs = (int)0.1 * round;
+        public void spawnMob() {
             List<String> XSpawns = new ArrayList<>();
             XSpawns.add("840.5");
             XSpawns.add("876.5");
             List<String> ZSpawns = new ArrayList<>();
             ZSpawns.add("-586.5");
             ZSpawns.add("-602.5");
+            List<String> EnemyTypes = new ArrayList<>();
+            EnemyTypes.add("minecraft:skeleton");
+            EnemyTypes.add("minecraft:zombie");
+            EnemyTypes.add("minecraft:creeper");
             // Take a random entry from both lists and combine them with Y 64 to make a Summon Command with random Entry from Mobs array
+            CommandArena obj = new CommandArena();
+            String randomX = obj.getRandomElementString(XSpawns);
+            String randomZ = obj.getRandomElementString(ZSpawns);
+            String randomEnemy = obj.getRandomElementString(EnemyTypes);
+            String spawnCommand = "minecraft:summon "+ randomEnemy +" "+ randomX +" 64 "+randomZ;
+        }
+        public void spawnWave() {
+            int maxMobs = ;
+            for (int i = 0; i < getRandomNumber(); i++){
 
-
-            System.out.println(round);
+            }
         }
         public void Wave() {
             // All 4 Waves
@@ -54,13 +77,13 @@ public final class Arena extends JavaPlugin {
             Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
 
             Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*10);
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*20);
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*20);
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*20);
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*30);
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*50);
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*70);
 
             if(ArenaMode) {
 
-                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> Wave(), 20*30);
+                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> Wave(), 20*110);
                 round++;
             }
         }
