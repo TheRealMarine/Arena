6614e662518e97388c82baa5875edd551e63c425
Patrick <panzercrafter0@gmail.com>
2022-04-23 21:13:38 +0200

    Entity targets Player + Players get registered to Array

--- src/main/java/me/pati/arena/Arena.java
+++ src/main/java/me/pati/arena/Arena.java
@@ -1,8 +1,6 @@
 package me.pati.arena;
 
 import java.lang.Math;
-import java.sql.Array;
-import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -13,9 +11,10 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -27,13 +26,9 @@ public final class Arena extends JavaPlugin {
     public class CommandArena implements CommandExecutor {
         boolean ArenaMode = false;
         int round = 0;
+        List<Player> players = new ArrayList<Player>();
+        int entityCount = 0;
         public Double getRandomElementDouble(List<Double> list)
-        {
-            Random r = new Random();
-            int randomitem = r.nextInt(list.size());
-            return list.get(randomitem);
-        }
-        public String getRandomElementString(List<String> list)
         {
             Random r = new Random();
             int randomitem = r.nextInt(list.size());
@@ -45,22 +40,47 @@ public final class Arena extends JavaPlugin {
             int randomitem = r.nextInt(list.size());
             return list.get(randomitem);
         }
+        public Player getRandomElementPlayer(List<Player> list)
+        {
+            Random r = new Random();
+            int randomitem = r.nextInt(list.size());
+            return list.get(randomitem);
+        }
         @Override
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-            if (!ArenaMode) {
-                ArenaMode = true;
-                round = 1;
-                String com = "minecraft:title @a title {\"text\":\"Arena Minigame was started!\",\"color\":\"green\"}";
-                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), com);
-                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> Wave(), 20*10);
-            } else {
-                ArenaMode = false;
-                String com = "minecraft:title @a title {\"text\":\"Arena Minigame was stopped!\",\"color\":\"green\"}";
-                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), com);
+            if (args.length == 0) {
+                if (!ArenaMode) {
+                    ArenaMode = true;
+                    entityCount = 0;
+                    round = 1;
+                    String com = "minecraft:title @a title {\"text\":\"Arena Minigame was started!\",\"color\":\"green\"}";
+                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), com);
+                    Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> Wave(), 20*10);
+                } else {
+                    ArenaMode = false;
+                    String com = "minecraft:title @a title {\"text\":\"Arena Minigame was stopped!\",\"color\":\"green\"}";
+                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), com);
+                }
+            } else if (Bukkit.getServer().getPlayer(args[0]) != null) { // If name is player, then register him to the arena
+                if (players.contains(Bukkit.getServer().getPlayer(args[0]))) {
+                    unRegisterArenaPlayer(Bukkit.getServer().getPlayer(args[0]));
+                } else {
+                    RegisterArenaPlayer(Bukkit.getServer().getPlayer(args[0]));
+                }
             }
             return false;
         }
-        public void spawnMobNew(PlayerInteractEvent e) {
+        public void RegisterArenaPlayer(Player player) {
+            players.add(player);
+        }
+        public void unRegisterArenaPlayer(Player player) {
+            players.remove(player);
+        }
+        public void targetRandomPlayer(Creature creature) {
+            Player player = getRandomElementPlayer(players);
+            creature.setTarget(player);
+        }
+        public void spawnMob() {
             List<Double> XSpawns = new ArrayList<>();
             XSpawns.add(840.5);
             XSpawns.add(876.5);
@@ -71,43 +91,21 @@ public final class Arena extends JavaPlugin {
             EntityTypes.add(EntityType.SKELETON);
             EntityTypes.add(EntityType.ZOMBIE);
             EntityTypes.add(EntityType.CREEPER);
-            // Take a random entry from both lists and combine them with Y 64 to make a Summon Command with random Entry from Mobs array
+
             CommandArena obj = new CommandArena();
             double randomX = obj.getRandomElementDouble(XSpawns);
             double randomZ = obj.getRandomElementDouble(ZSpawns);
             EntityType randomEnemy = obj.getRandomElementEntityType(EntityTypes);
+
             World world = Bukkit.getWorld("world");
             Location l = new Location(world, randomX, 64, randomZ);
-            e.getPlayer().getWorld().spawnEntity(l, randomEnemy);
-        }
-        public void spawnMob() {
-            List<String> XSpawns = new ArrayList<>();
-            XSpawns.add("840.5");
-            XSpawns.add("876.5");
-            List<String> ZSpawns = new ArrayList<>();
-            ZSpawns.add("-586.5");
-            ZSpawns.add("-602.5");
-            List<String> EnemyTypes = new ArrayList<>();
-            EnemyTypes.add("minecraft:skeleton");
-            EnemyTypes.add("minecraft:zombie");
-            EnemyTypes.add("minecraft:creeper");
-            // Take a random entry from both lists and combine them with Y 64 to make a Summon Command with random Entry from Mobs array
-            CommandArena obj = new CommandArena();
-            String randomX = obj.getRandomElementString(XSpawns);
-            String randomZ = obj.getRandomElementString(ZSpawns);
-            String randomEnemy = obj.getRandomElementString(EnemyTypes);
-            //System.out.println(randomX);
-            //System.out.println(randomZ);
-            //System.out.println(randomEnemy);
-            String spawnCommand = "minecraft:summon "+ randomEnemy +" "+ randomX +" 64 "+randomZ;
-            //System.out.println(spawnCommand);
-            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), spawnCommand);
+            Creature entity = (Creature) world.spawnEntity(l, randomEnemy);
+            targetRandomPlayer(entity); // Moves to random player
+            //entityCount++; // TODO: Count entity deaths to spawn next wave. Listen to onDeath events or smth and check for entity spawned by the Arena and then decrease count.
         }
         public void spawnWave() {
             int minMobs = (int)(10 * (0.2 * round));
             int maxMobs = (int)(10 * (0.4 * round));
-            //System.out.println(minMobs);
-            //System.out.println(maxMobs);
             for (int i = 0; i < getRandomNumber(minMobs, maxMobs); i++){
                 spawnMob();
             }
