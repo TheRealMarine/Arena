53ef9917cb7223933a9784f5be69e253a154c41f
Patrick <panzercrafter0@gmail.com>
2022-04-23 19:49:11 +0200

    Redone Entity Spawn

--- src/main/java/me/pati/arena/Arena.java
+++ src/main/java/me/pati/arena/Arena.java
@@ -8,10 +8,14 @@ import java.util.List;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -23,12 +27,19 @@ public final class Arena extends JavaPlugin {
     public class CommandArena implements CommandExecutor {
         boolean ArenaMode = false;
         int round = 0;
-        public int getRandomElement(List<Integer> list)
+        public Double getRandomElementDouble(List<Double> list)
         {
-            Random rand = new Random();
-            return list.get(rand.nextInt(list.size()));
+            Random r = new Random();
+            int randomitem = r.nextInt(list.size());
+            return list.get(randomitem);
         }
         public String getRandomElementString(List<String> list)
+        {
+            Random r = new Random();
+            int randomitem = r.nextInt(list.size());
+            return list.get(randomitem);
+        }
+        public EntityType getRandomElementEntityType(List<EntityType> list)
         {
             Random r = new Random();
             int randomitem = r.nextInt(list.size());
@@ -39,15 +50,35 @@ public final class Arena extends JavaPlugin {
             if (!ArenaMode) {
                 ArenaMode = true;
                 round = 1;
-                String com = "minecraft:title @a title {\"text\":\"Arena Minigame wurde aktiviert!\",\"color\":\"yellow\"}";
+                String com = "minecraft:title @a title {\"text\":\"Arena Minigame was started!\",\"color\":\"green\"}";
                 Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), com);
-                Wave();
+                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> Wave(), 20*10);
             } else {
                 ArenaMode = false;
-                String com = "minecraft:title @a title {\"text\":\"Arena Minigame wurde deaktiviert!\",\"color\":\"yellow\"}";
+                String com = "minecraft:title @a title {\"text\":\"Arena Minigame was stopped!\",\"color\":\"green\"}";
                 Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), com);
             }
             return false;
+        }
+        public void spawnMobNew(PlayerInteractEvent e) {
+            List<Double> XSpawns = new ArrayList<>();
+            XSpawns.add(840.5);
+            XSpawns.add(876.5);
+            List<Double> ZSpawns = new ArrayList<>();
+            ZSpawns.add(-586.5);
+            ZSpawns.add(-602.5);
+            List<EntityType> EntityTypes = new ArrayList<>();
+            EntityTypes.add(EntityType.SKELETON);
+            EntityTypes.add(EntityType.ZOMBIE);
+            EntityTypes.add(EntityType.CREEPER);
+            // Take a random entry from both lists and combine them with Y 64 to make a Summon Command with random Entry from Mobs array
+            CommandArena obj = new CommandArena();
+            double randomX = obj.getRandomElementDouble(XSpawns);
+            double randomZ = obj.getRandomElementDouble(ZSpawns);
+            EntityType randomEnemy = obj.getRandomElementEntityType(EntityTypes);
+            World world = Bukkit.getWorld("world");
+            Location l = new Location(world, randomX, 64, randomZ);
+            e.getPlayer().getWorld().spawnEntity(l, randomEnemy);
         }
         public void spawnMob() {
             List<String> XSpawns = new ArrayList<>();
@@ -81,20 +112,23 @@ public final class Arena extends JavaPlugin {
                 spawnMob();
             }
         }
+        public void nextWave() {
+            if(ArenaMode) {
+                round++;
+                Wave();
+            }
+        }
         public void Wave() {
             // All 4 Waves
             String command = "minecraft:title @a title {\"text\":\"Wave: "+ round +"\",\"color\":\"dark_red\"}";
             Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
 
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*10);
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*40);
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*80);
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*120);
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*5);
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*(15*((long)(1+(.3*round)))));
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*(25*((long)(1+(.3*round)))));
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> spawnWave(), 20*(35*((long)(1+(.3*round)))));
 
-            if(ArenaMode) {
-                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> Wave(), 20*160);
-                round++;
-            }
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, () -> nextWave(), 20*(50*((long)(1+(.3*round)))));
         }
     }
 
